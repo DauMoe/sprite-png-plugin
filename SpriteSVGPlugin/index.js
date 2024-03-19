c98e37e6e05c4cd70914319155a0f37fc8cbf961
Hoàng Lê <hoang.lehuy@positivethinking.tech>
2024-03-19 23:57:04 +0700

    Create manifest file (coordinate mapping)

--- SpriteSVGPlugin/index.js
+++ SpriteSVGPlugin/index.js
@@ -12,25 +12,32 @@ const PACKAGE_NAME = "SpriteSVGPlugin";
  *  - Compilation docs: https://webpack.js.org/api/compilation-hooks/
  */
 
-const isSVG = fileName => path.extname(path.basename(fileName)) === '.svg';
-const delFolder = util.promisify(fs.unlink);
 
-const getConfig = options => ({
-  // dest: options.outputPath ?? path.join(__dirname, "test"),
-  // // log: "debug"
-  // mode: {
-  //   css: {
-  //     bust: false,
-  //   }
-  // }
-})
 
-const genCoordinateMap = (svgData) => {
-  let { path: filePath, contents } = svgData;
-  // contents = contents.replace("\ufeff", ""); //remove non-whitespace
-  xml2js.parseString(contents, { trim: true }, (err, result) => {
+const isSVG = fileName => fileName?.endsWith(".svg");
+
+const genCoordinateMap = (svgData, outputDir, manifestFileName = "manifest.json") => {
+  let { contents } = svgData;
+  xml2js.parseString(contents, (err, result) => {
     if (err) throw Error(err);
-    console.log("DATA", result.svg.svg[0].path);
+    if (!manifestFileName?.endsWith(".json")) manifestFileName += ".json";
+
+    let coordData = {};
+    const { $, view, svg } = result.svg;
+    coordData["totalWidth"] = +$.width;
+    coordData["totalHeight"] = +$.height;
+    view.forEach((data, index) => {
+      const svgInfo = svg[index].$;
+      const { id, viewBox } = data.$;
+      coordData[id] = {
+        ...svgInfo,
+        x: +svgInfo.x || 0,
+        y: +svgInfo.y || 0,
+        width: +svgInfo.width || 0,
+        height: +svgInfo.height || 0,
+      }
+    });
+    fs.writeFile(path.join(outputDir, manifestFileName), JSON.stringify(coordData), "utf-8", () => {});
   })
 }
 
@@ -39,15 +46,13 @@ module.exports = class SpriteSVGPlugin {
     // this.options = options;
     // this.includes = options.includes || [];
     // this.excludes = options.excludes || [];
-    this.outputPath = options.outputPath ?? path.join(__dirname, "test")
+    this.outputPath = options.outputPath ?? path.join(__dirname, "test");
+    this.manifestFileName = options.manifestFileName || undefined;
     this.spriter = new SVGSpriter({
-      // shape: {
-      //   dest: this.outputPath
-      // }
       dest: this.outputPath,
       mode: {
         view: {
-          bust: false
+          bust: false,
         }
       }
     });
@@ -65,7 +70,7 @@ module.exports = class SpriteSVGPlugin {
         for (const mode of Object.values(result)) {
           for (const resource of Object.values(mode)) {
             // console.log("RE", resource.contents.toString());
-            genCoordinateMap(resource);
+            genCoordinateMap(resource, this.outputPath, this.manifestFileName);
             // fs.mkdirSync(path.dirname(resource.path), { recursive: true });
             // fs.writeFileSync(resource.path, resource.contents);
           }
