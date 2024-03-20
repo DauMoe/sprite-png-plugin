aa871ed88288f99bbc76371faa2c32949377b940
Hoàng Lê <hoang.lehuy@positivethinking.tech>
2024-03-21 01:01:30 +0700

    Add issue

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const { sources } = require("webpack");
 const spritesheet = require("spritesheet-js");
 
 const _PACKAGE_NAME = "SpritePNG_Plugin";
@@ -13,30 +10,41 @@ const _PACKAGE_NAME = "SpritePNG_Plugin";
 
 module.exports = class SpritePNG_Plugin {
   constructor(options = {}) {
-    // this.includes             = options.includes; // allow regex and array
-    // this.excludes             = options.excludes; // allow regex and array
+    this.relativePaths        = options.relativePaths; // list relative png paths that will be combine into sprite
     this.outputDir            = options.outputDir;
     // this.manifestFileName     = this.getManifestFileName(options.manifestFileName);
-    this.spriteFileName       = options.spriteFileName;
+    this.spriteSheetName      = options.spriteSheetName;
     // this.relativeManifestPath = path.join(this.outputDir, this.manifestFileName);
     // this.relativeSpritePath   = path.join(this.outputDir, this.spriteFileName);
   }
 
+  isPng = (filePath) => filePath?.endsWith('.png');
 
   apply(compiler) {
     compiler.hooks.thisCompilation.tap({ name: _PACKAGE_NAME }, (compilation) => {
-      //Start creating SVG sprite sheet after all assets are move to chunk
+      // Start creating SVG sprite sheet after all assets are move to chunk
       compilation.hooks.processAssets.tap(
         {
           name: _PACKAGE_NAME,
-          stage: "PROCESS_ASSETS_STAGE_PRE_PROCESS"
+          stage: "PROCESS_ASSETS_STAGE_OPTIMIZE"
         },
         (assets) => {
+          /**
+           * @NOTE
+           *  - EXEC get from "node:child-process" instead of "platform-command"
+           */
+
+          /**
+           * @todo
+           *  - [ ] Get relative path from assets or chunks instead of pass as prop
+           *  - [ ] How can I import sprite and json into js file if they are emitted to asset in webpack hooks?
+           */
+
           // Lib option: https://github.com/krzysztof-o/spritesheet.js/blob/master/index.js#L166
-          spritesheet(Object.keys(assets), {
+          spritesheet(this.relativePaths.filter(this.isPng), {
             format: "json",
             path: this.outputDir,
-            name: this.spriteFileName
+            name: this.spriteSheetName
           }, err => {
             if (err) throw err;
             console.log("________ GENERATED _________");
