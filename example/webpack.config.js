42310386908540d9c5bb9726de0f0f274d0e5555
le_hoang <hoang.lehuy@positivethinking.tech>
2024-03-20 13:26:22 +0700

    refactor structure

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SpriteSVGPlugin = require("./SpriteSVGPlugin");
+const SpriteSVGPlugin = require("./../index");
 
 const subStylePaths = [];
 
